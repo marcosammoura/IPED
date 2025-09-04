f76d10c55813a92766e549d7d45c6604cba71fa6
Marcos Moura <marcosammoura@gmail.com>
2025-09-03 22:34:22 -0300

    Task code foundation

---
+++ iped-app/resources/scripts/tasks/AgeEstimationTask.py
@@ -0,0 +1,12 @@
+﻿'''
+# Age estimation feature based on Open Age Detection project (https://huggingface.co/prithivMLmods/open-age-detection)
+# AgeEstimationTask.py - By Marcos Moura and Luis Nassif
+# Requirements: See https://github.com/sepinf-inc/IPED/wiki/User-Manual#AgeEstimation
+'''
+
+# configuration properties
+enableProp = 'enableAgeEstimation'
+configFile = 'AgeEstimationConfig.txt'
+numAgeEstimationProcessesProp = 'numAgeEstimationProcesses'
+categorizationThresholdProp = 'categorizationThreshold'
+
